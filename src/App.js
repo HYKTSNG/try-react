c414d6d314c7d3c22ddf03f4be93026daca1992d
HYKTSNG <Sin5hayak@gmail.com>
2023-03-26 16:33:06 +0900

    add: input

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import MyComponent from './helloWorld/helloWorld';
+import InputComponent from './imputTest/inputTest';
 
 function App() {
   return (
@@ -17,6 +19,12 @@ function App() {
         >
           Learn React
         </a>
+        <div>
+          <MyComponent />
+        </div>
+        <div>
+          <InputComponent />
+        </div>
       </header>
     </div>
   );
