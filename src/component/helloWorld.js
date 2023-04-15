0e6d193c793cf6af3c3e418252e47e897c4cd868
HYKTSNG <Sin5hayak@gmail.com>
2023-04-15 19:21:01 +0900

    update: 画像を表示し2秒ごとにランダムに場所変更

--- src/component/helloWorld.js
+++ src/component/helloWorld.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import dog1 from "../Images/dog1.jpg";
 import dog2 from "../Images/dog2.jpg";
 import dog3 from "../Images/dog3.jpg";
@@ -12,24 +12,33 @@ const styles = {
   },
 };
 
+function shuffle(array) {
+  // Fisher-Yatesアルゴリズムを使ったランダムシャッフル
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
 function HelloWorld() {
+  const [images, setImages] = useState([dog1, dog2, dog3, dog4, dog5]);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setImages((prevImages) => shuffle([...prevImages]));
+    }, 2000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <div>
-      <span>
-        <img src={dog1} alt="imageTest" style={styles.imageStyle} />
-      </span>
-      <span>
-        <img src={dog2} alt="imageTest" style={styles.imageStyle} />
-      </span>
-      <span>
-        <img src={dog3} alt="imageTest" style={styles.imageStyle} />
-      </span>
-      <span>
-        <img src={dog4} alt="imageTest" style={styles.imageStyle} />
-      </span>
-      <span>
-        <img src={dog5} alt="imageTest" style={styles.imageStyle} />
-      </span>
+      {images.map((image, index) => (
+        <span key={index}>
+          <img src={image} alt="imageTest" style={styles.imageStyle} />
+        </span>
+      ))}
     </div>
   );
 }
